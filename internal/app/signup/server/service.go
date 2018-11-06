362c112aad873b81461a728cad31bd8c04aa10cb
Daniel Higuero <daniel@daisho.group>
2018-11-06 11:04:45 +0100

    NP-256 Create signup skeleton

---
+++ internal/app/signup/server/service.go
@@ -0,0 +1,82 @@
+package server
+
+import (
+	"fmt"
+	"github.com/nalej/derrors"
+	"github.com/nalej/grpc-organization-go"
+	"github.com/nalej/grpc-user-manager-go"
+	"github.com/nalej/grpc-utils/pkg/tools"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+)
+
+type Service struct {
+	Configuration Config
+	Server * tools.GenericGRPCServer
+}
+
+// NewService creates a new system model service.
+func NewService(conf Config) *Service {
+	return &Service{
+		conf,
+		tools.NewGenericGRPCServer(uint32(conf.Port)),
+	}
+}
+
+type Clients struct {
+	orgClient grpc_organization_go.OrganizationsClient
+	userClient grpc_user_manager_go.UserManagerClient
+}
+
+func (s * Service) GetClients() (* Clients, derrors.Error) {
+	smConn, err := grpc.Dial(s.Configuration.SystemModelAddress, grpc.WithInsecure())
+	if err != nil{
+		return nil, derrors.AsError(err, "cannot create connection with the system model")
+	}
+
+	uConn, err := grpc.Dial(s.Configuration.UserManagerAddress, grpc.WithInsecure())
+	if err != nil{
+		return nil, derrors.AsError(err, "cannot create connection with the user manager")
+	}
+
+	oClient := grpc_organization_go.NewOrganizationsClient(smConn)
+	uClient := grpc_user_manager_go.NewUserManagerClient(uConn)
+
+	return &Clients{oClient, uClient}, nil
+}
+
+// Run the service, launch the REST service handler.
+func (s *Service) Run() error {
+	vErr := s.Configuration.Validate()
+	if vErr != nil{
+		log.Fatal().Str("err", vErr.DebugReport()).Msg("invalid configuration")
+	}
+
+	s.Configuration.Print()
+
+	return s.LaunchGRPC()
+}
+
+func (s * Service) LaunchGRPC() error {
+	_, cErr := s.GetClients()
+	if cErr != nil{
+		log.Fatal().Str("err", cErr.DebugReport()).Msg("cannot generate clients")
+		return cErr
+	}
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", s.Configuration.Port))
+	if err != nil {
+		log.Fatal().Errs("failed to listen: %v", []error{err})
+	}
+
+	grpcServer := grpc.NewServer()
+
+	// Register reflection service on gRPC server.
+	reflection.Register(grpcServer)
+	log.Info().Int("port", s.Configuration.Port).Msg("Launching gRPC server")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal().Errs("failed to serve: %v", []error{err})
+	}
+	return nil
+}
