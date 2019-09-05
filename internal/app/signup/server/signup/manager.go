348f0deafeb80365e0e23e0e2d5412f11aab9374
Marcos Garcia <mgarcia@nalej.com>
2019-09-05 15:34:52 +0200

    NP-1914 Create new default role NalejAdmin (and let the linter perform a cleanup)

--- internal/app/signup/server/signup/manager.go
+++ internal/app/signup/server/signup/manager.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)  2018 Nalej - All Rights Reserved
+ * Copyright (C) 2019 Nalej - All Rights Reserved
  */
 
 package signup
@@ -7,32 +7,37 @@ package signup
 import (
 	"context"
 	"github.com/nalej/derrors"
+	"github.com/nalej/grpc-application-go"
 	"github.com/nalej/grpc-authx-go"
 	"github.com/nalej/grpc-common-go"
-	"github.com/nalej/grpc-application-go"
+	"github.com/nalej/grpc-infrastructure-go"
 	"github.com/nalej/grpc-organization-go"
 	"github.com/nalej/grpc-signup-go"
 	"github.com/nalej/grpc-user-manager-go"
-	"github.com/nalej/grpc-infrastructure-go"
 	"github.com/nalej/grpc-utils/pkg/conversions"
 	"github.com/rs/zerolog/log"
 )
 
 // DefaultRoles defines the map of roles that will be automatically created by the system by default.
 var DefaultRoles = map[string][]grpc_authx_go.AccessPrimitive{
-	"Owner": []grpc_authx_go.AccessPrimitive{
+	"Owner": {
 		grpc_authx_go.AccessPrimitive_ORG,
 	},
-	"Operator": []grpc_authx_go.AccessPrimitive{
+	"Operator": {
 		grpc_authx_go.AccessPrimitive_PROFILE,
 		grpc_authx_go.AccessPrimitive_RESOURCES,
 	},
-	"Developer": []grpc_authx_go.AccessPrimitive{
+	"Developer": {
 		grpc_authx_go.AccessPrimitive_PROFILE,
 		grpc_authx_go.AccessPrimitive_APPS,
 	},
-	"AppCluster": []grpc_authx_go.AccessPrimitive{
+	"AppCluster": {
 		grpc_authx_go.AccessPrimitive_APPCLUSTEROPS,
+	},
+	"NalejAdmin": {
+		grpc_authx_go.AccessPrimitive_ORG,
+		grpc_authx_go.AccessPrimitive_ORGMNGT,
+		grpc_authx_go.AccessPrimitive_RESOURCESMNGT,
 	},
 }
 
@@ -42,14 +47,15 @@ var InternalRoles = map[string]bool{
 	"Operator":   false,
 	"Developer":  false,
 	"AppCluster": true,
+	"NalejAdmin": true,
 }
 
 // Manager structure with the required providers for cluster operations.
 type Manager struct {
-	OrgClient  grpc_organization_go.OrganizationsClient
-	UserClient grpc_user_manager_go.UserManagerClient
+	OrgClient     grpc_organization_go.OrganizationsClient
+	UserClient    grpc_user_manager_go.UserManagerClient
 	ClusterClient grpc_infrastructure_go.ClustersClient
-	AppClient grpc_application_go.ApplicationsClient
+	AppClient     grpc_application_go.ApplicationsClient
 }
 
 // NewManager creates a Manager using a set of providers.
@@ -58,7 +64,7 @@ func NewManager(
 	userClient grpc_user_manager_go.UserManagerClient,
 	clusterClient grpc_infrastructure_go.ClustersClient,
 	appClient grpc_application_go.ApplicationsClient,
-	) Manager {
+) Manager {
 	return Manager{orgClient, userClient, clusterClient, appClient}
 }
 
@@ -124,66 +130,66 @@ func (m *Manager) createRoles(organizationID string) (*string, error) {
 }
 
 // ListOrganizations returns the list of organizations in the system.
-func (m *Manager) ListOrganizations(request *grpc_signup_go.SignupInfoRequest) (*grpc_signup_go.OrganizationsList, error){
+func (m *Manager) ListOrganizations(request *grpc_signup_go.SignupInfoRequest) (*grpc_signup_go.OrganizationsList, error) {
 	orgs, err := m.OrgClient.ListOrganizations(context.Background(), &grpc_common_go.Empty{})
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	result := make([]*grpc_signup_go.OrganizationInfo, 0, len(orgs.Organizations))
-	for _, org := range orgs.Organizations{
+	for _, org := range orgs.Organizations {
 		info, err := m.extendOrganizationInfo(org)
-		if err != nil{
+		if err != nil {
 			return nil, err
 		}
 		result = append(result, info)
 	}
 	return &grpc_signup_go.OrganizationsList{
-		Organizations:        result,
+		Organizations: result,
 	}, err
 }
 
-func (m*Manager) extendOrganizationInfo(org * grpc_organization_go.Organization) (*grpc_signup_go.OrganizationInfo, error){
+func (m *Manager) extendOrganizationInfo(org *grpc_organization_go.Organization) (*grpc_signup_go.OrganizationInfo, error) {
 	orgID := &grpc_organization_go.OrganizationId{
-		OrganizationId:       org.OrganizationId,
+		OrganizationId: org.OrganizationId,
 	}
 	users, err := m.UserClient.ListUsers(context.Background(), orgID)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	clusters, err := m.ClusterClient.ListClusters(context.Background(), orgID)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	descriptors, err := m.AppClient.ListAppDescriptors(context.Background(), orgID)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	instances, err := m.AppClient.ListAppInstances(context.Background(), orgID)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return &grpc_signup_go.OrganizationInfo{
-		OrganizationId:       org.OrganizationId,
-		Name:                 org.Name,
-		Created:              org.Created,
-		NumberUsers:          int32(len(users.Users)),
-		NumberClusters:       int32(len(clusters.Clusters)),
-		NumberDescriptors:    int32(len(descriptors.Descriptors)),
-		NumberInstances:      int32(len(instances.Instances)),
+		OrganizationId:    org.OrganizationId,
+		Name:              org.Name,
+		Created:           org.Created,
+		NumberUsers:       int32(len(users.Users)),
+		NumberClusters:    int32(len(clusters.Clusters)),
+		NumberDescriptors: int32(len(descriptors.Descriptors)),
+		NumberInstances:   int32(len(instances.Instances)),
 	}, nil
 }
 
 // GetOrganizationInfo retrieves the information about an organization.
-func (m *Manager) GetOrganizationInfo(organizationID *grpc_organization_go.OrganizationId) (*grpc_signup_go.OrganizationInfo, error){
+func (m *Manager) GetOrganizationInfo(organizationID *grpc_organization_go.OrganizationId) (*grpc_signup_go.OrganizationInfo, error) {
 	org, err := m.OrgClient.GetOrganization(context.Background(), organizationID)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return m.extendOrganizationInfo(org)
 }
 
 // DeleteOrganization removes an organization from the system.
-func (m *Manager) RemoveOrganization(organizationID *grpc_organization_go.OrganizationId) error{
+func (m *Manager) RemoveOrganization(organizationID *grpc_organization_go.OrganizationId) error {
 	log.Info().Str("organizationID", organizationID.OrganizationId).Msg("Removing organization")
 	// Undeploy running apps
 	// Delete descriptors
